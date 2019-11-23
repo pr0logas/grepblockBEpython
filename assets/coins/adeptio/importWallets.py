b648dd77d90eb1f72f6c64561f6365a70dbce49e
pr0logas <prologas@protonmail.com>
2019-11-23 19:18:16 +0200

    Update

--- assets/coins/adeptio/importWallets.py
+++ assets/coins/adeptio/importWallets.py
@@ -6,9 +6,15 @@ sys.path.append('../../')
 from mongoDB import *
 from parseWallets import * 
 from performance import perfResult, setTimeCommand, time
+from iquidusExplorer import *
 
 collection = "txidsProgress"
 setProcStart = setTimeCommand
+
+# Init Explorer JSON data
+ED = iquidusExplorer()
+print ED.getBlockHash('5')
+print ED.getBlockContentByHash('00000ffa49117a1763cbd558eab797dd6f046acf3d058f4ce1ee1ab53e926191')
 
 # Init MongoConnection
 MC = mongoConnection(mongoAuth, database, collection)
