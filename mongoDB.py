03c1ab6238f83b82fdc7b1a327498418a426ec1f
pr0logas <prologas@protonmail.com>
2019-11-23 15:58:57 +0200

    New code

--- mongoDB.py
+++ mongoDB.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
+"""
+Install modules:
+	apt install python-pip
+	pip install pymongo
+"""
+
 import pymongo
+from pymongo.errors import AutoReconnect
 
 mongoAuth = {
 	"host" : "127.0.0.1",
@@ -15,12 +22,33 @@ class mongoConnection():
 		self.mongoDB = self.mongoConn[mongoAuth["db"]]
 		self.mongoCol = self.mongoDB[mongoAuth["col"]]
 
-	def findBlocks(self, blockNum):
+	def autoreconnect_retry(fn, retries=10):
+	    def db_op_wrapper(*args, **kwargs):
+	        tries = 0
+
+	        while tries < retries:
+	            try:
+	                return fn(*args, **kwargs)
+
+	            except AutoReconnect:
+	                tries += 1
+
+	        raise Exception("MongoDB not responding. No luck even after %d retries" % retries)
+
+	    return db_op_wrapper
+
+	@autoreconnect_retry
+	def findByBlock(self, blockNum):
 		searchBlock = list(self.mongoCol.find({'block' : blockNum}))
 		return searchBlock
 
+	@autoreconnect_retry
+	def findByTx(self, blockTx):
+		searchTx = list(self.mongoCol.find({'tx' : blockTx}))
+		return searchTx
 
-MC = mongoConnection(mongoAuth)
-result = MC.findBlocks(1);
-
-print result
+	@autoreconnect_retry
+	def findLastBlock(self):
+		searchLastBlock = list(self.mongoCol.find({},{ "_id": 0, "block": 1}).sort([( '$natural', -1 )] ).limit(1))
+		lastBlock = searchLastBlock[0]['block']
+  		return lastBlock
--- parseWallets.py
+++ parseWallets.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
-"""
-Install modules:
-	apt install python-pip
-	pip install pymongo
-"""
-
 import datetime
-from mongoDB import mongoAuth
+from mongoDB import mongoAuth, mongoConnection
 
-print mongoAuth
+MC = mongoConnection(mongoAuth)
+print MC.findByBlock(2);
+print MC.findByTx('49f40027e05960fe15f58874926331b170a9af1ff8ccee28ee0c9161ae8c9f88');
+print MC.findLastBlock()
+
+
