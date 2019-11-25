c8ec705609e2977596b2b4fa16fbe9f650a7b9f8
Mister Nemo <miister.nemo@gmail.com>
2019-11-25 10:40:39 +0000

    coinImportBlocks.py edited online with Bitbucket

--- assets/coins/adeptio/coinImportBlocks.py
+++ assets/coins/adeptio/coinImportBlocks.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python
 
 import sys, time
-from adeptio import *
+from coin import *
 sys.path.append('../../../')
 from mongoDB import *
 from parseWallets import aggregateWalletsData
 from explorer import iquidusExplorer
 
+for coin in coins:
 
-collectionForBlocks = "blocks"
+	collectionForBlocks = "blocks"
 
-# Init Mongo Connection to Class;
-MC = mongoConnection(mongoAuth, database, collectionTxidProgress)
+	# Init Mongo Connection to Class;
+	MC = mongoConnection(mongoAuth, coin.database, collectionTxidProgress)
 
-# Set current progress;
-currentLastBlock = MC.findLastBlock(collectionForBlocks)
+	# Set current progress;
+	currentLastBlock = MC.findLastBlock(collectionForBlocks)
 
-# Init Explorer params to Class;
-EX = iquidusExplorer(chainProvider, getBlockIndexMethod, getBlockwithHashMethod, getTx)
+	# Init Explorer params to Class;
+	EX = iquidusExplorer(coin.chainProvider, coin.getBlockIndexMethod, coin.getBlockwithHashMethod, coin.getTx)
 
-# Init Data Aggregation Class;
-AG = aggregateWalletsData()
+	# Init Data Aggregation Class;
+	AG = aggregateWalletsData()
 
-# Check if blocks col empty or not?
-print MC.checkIfBlocksColEmpty(collectionForBlocks)
+	# Check if blocks col empty or not?
+	print MC.checkIfBlocksColEmpty(collectionForBlocks)
