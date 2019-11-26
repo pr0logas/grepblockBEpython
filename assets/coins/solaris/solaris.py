d82e03afd8fe3b8fa82c521b7494e28b25fac5fd
Tomas Andriekus <tomas@dappradar.com>
2019-11-26 15:54:45 +0200

    Launch solaris XLR

---
+++ assets/coins/solaris/solaris.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# Defaults
+database = 'solaris'
+assetTicker = 'XLR'
+assetName = 'solaris'
+genesisBlock = 1572355036
+coinGeckoStartUnixTime = 1512864000
+blockTime = 60
+
+# RPC
+rpcconnect = '10.10.100.201'
+rpcport = 1111
+rpcuser = 'grepblock'
+rpcpassword = 'tothemoon'
+
+parseBlocksInRangeFor = 99
+
+chainProvider = 'https://explorer.solarisplatform.com'
+getBlockHashMethod = '/api/getblockhash?index='
+getBlockwithHashMethod = '/api/getblock?hash='
+getTx = '/api/getrawtransaction?txid='
+
+apiProvider = 'api.coingecko.com'
+
+#
+daemonCli="solaris-cli"
