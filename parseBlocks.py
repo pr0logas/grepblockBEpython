d044f1fe34752889afa6b6e44bf151ebf1aae31d
pr0logas <prologas@protonmail.com>
2019-11-26 20:41:56 +0200

    Trying to solve Solaris issue

--- parseBlocks.py
+++ parseBlocks.py
@@ -17,7 +17,8 @@ class aggregateBlocksData():
 		if check == True:
 			firstObj['block'] = int(findBlockNum)
 			firstObj['time'] = int(firstObj['time'])
-			firstObj['mediantime'] = int(firstObj['mediantime'])
+			if 'mediantime' in firstObj:
+				firstObj['mediantime'] = int(firstObj['mediantime'])
 			return json.dumps(firstObj)
 		else:
 			print "FAIL! Looks like we can't aggregate Block Number!?"
