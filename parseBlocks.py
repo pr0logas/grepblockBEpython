f7a9ea105e164162c9e19f99c4ebe7565e11ece5
Tomas Andriekus <tomas@dappradar.com>
2019-11-26 17:25:38 +0200

    Solaris gives not a valid content

--- parseBlocks.py
+++ parseBlocks.py
@@ -8,16 +8,13 @@ class aggregateBlocksData():
 
 	def aggregateInsertBlockNumber(self, blockData):
 		originalData = blockData
-		print originalData
 		firstObj = json.loads(blockData)
 		findBlockNum = int(firstObj['height'])
 		check = isinstance(findBlockNum, (int, long))
 
 		if check == True:
 			append = '{\n  "block" : ' + str(findBlockNum) + ','
-			print append
 			inserted = originalData.replace('{', append)
-			print inserted
 			return inserted
 		else:
 			print "FAIL! Looks like we can't aggregate Block Number!?"
