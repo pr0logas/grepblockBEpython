103484cd08c597c49a1ae632f27b4b00b93bda91
pr0logas <prologas@protonmail.com>
2019-11-28 20:57:02 +0200

    DigiByte cant get lastBlock

---
+++ assets/coins/digibyte/digibyte.py
@@ -0,0 +1,25 @@
+# Defaults
+database = 'digibyte'
+assetTicker = 'DGB'
+assetName = 'digibyte'
+genesisBlock = 1389392876
+coinGeckoStartUnixTime = 1391644800
+blockTime = 15
+
+# RPC
+rpcconnect = '10.10.100.201'
+rpcport = 11111
+rpcuser = 'grepblockuser'
+rpcpassword = 'grepblocktothemoon'
+
+parseBlocksInRangeFor = 99
+
+chainProvider = 'https://digiexplorer.info/'
+getBlockIndexMethod = 'api/block-index/'
+getBlockwithHashMethod = 'api/block/'
+getTx = 'api/tx/'
+
+apiProvider = 'api.coingecko.com'
+
+#
+daemonCli = "digibyte-cli"
