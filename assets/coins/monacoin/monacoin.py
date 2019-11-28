7564ee87921869d703bf609bcbc8f347d9beb5a7
pr0logas <prologas@protonmail.com>
2019-11-28 21:10:23 +0200

    MonaCoin cant get last Block

---
+++ assets/coins/monacoin/monacoin.py
@@ -0,0 +1,25 @@
+# Defaults
+database = 'monacoin'
+assetTicker = 'MONA'
+assetName = 'monacoin'
+genesisBlock = 1388534484
+coinGeckoStartUnixTime = 1395273600
+blockTime = 90
+
+# RPC
+rpcconnect = '10.10.100.201'
+rpcport = 11111
+rpcuser = 'grepblockuser'
+rpcpassword = 'grepblocktothemoon'
+
+parseBlocksInRangeFor = 99
+
+chainProvider = 'https://mona.chainsight.info/'
+getBlockIndexMethod = 'api/block-index/'
+getBlockwithHashMethod = 'api/block/'
+getTx = 'api/tx/'
+
+apiProvider = 'api.coingecko.com'
+
+#
+daemonCli = "monacoin-cli"
