0cdac4bfae3579e7afe0eeb695c8233760bab9dd
pr0logas <prologas@protonmail.com>
2019-11-29 00:17:31 +0200

    Pivx added

---
+++ assets/coins/pivx/pivx.py
@@ -0,0 +1,27 @@
+
+
+# Defaults
+database = "pivx"
+assetTicker="PIVX"
+assetName = "pivx"
+genesisBlock = 1454127007
+coinGeckoStartUnixTime = 1455494400
+blockTime = 60
+
+# RPC
+rpcconnect = "10.10.100.201"
+rpcport = 51473
+rpcuser = "grepblock"
+rpcpassword = "tothemoon"
+
+parseBlocksInRangeFor = 99
+
+chainProvider = 'https://mainnet-explorer.pivx.org'
+getBlockIndexMethod = '/api/getblockhash?index='
+getBlockwithHashMethod = '/api/getblock?hash='
+getTx = '/api/getrawtransaction?txid='
+
+apiProvider = 'api.coingecko.com'
+
+#
+daemonCli = "pivx-cli"
