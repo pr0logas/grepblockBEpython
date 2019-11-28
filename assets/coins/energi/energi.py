06394bc300919d3455d9ad6874a82a408001eb41
pr0logas <prologas@protonmail.com>
2019-11-29 00:00:12 +0200

    Energi added

---
+++ assets/coins/energi/energi.py
@@ -0,0 +1,25 @@
+# Defaults
+database = "energi"
+assetTicker="NRG"
+assetName = "energi"
+genesisBlock = 1523721282
+coinGeckoStartUnixTime = 1535846400
+blockTime = 60
+
+# RPC
+rpcconnect = "10.10.100.201"
+rpcport = 1111
+rpcuser = "grepblock"
+rpcpassword = "tothemoon"
+
+parseBlocksInRangeFor = 99
+
+chainProvider = 'https://explorer.energi.network'
+getBlockIndexMethod = '/api/getblockhash?index='
+getBlockwithHashMethod = '/api/getblock?hash='
+getTx = '/api/getrawtransaction?txid='
+
+apiProvider = 'api.coingecko.com'
+
+#
+daemonCli = "energi-cli"
