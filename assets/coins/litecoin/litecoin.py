326a0ce2b3cc0e64978872dbec449e21b9ca8c23
pr0logas <prologas@protonmail.com>
2019-11-28 21:02:31 +0200

    LTC added

---
+++ assets/coins/litecoin/litecoin.py
@@ -0,0 +1,25 @@
+# Defaults
+database = "litecoin"
+assetTicker="LTC"
+assetName = "litecoin"
+genesisBlock = 1368576000
+coinGeckoStartUnixTime = 1368576000
+blockTime = 150
+
+# RPC
+rpcconnect = "10.10.100.201"
+rpcport = 9332
+rpcuser = "grepblock"
+rpcpassword = "tothemoon"
+
+parseBlocksInRangeFor = 99
+
+chainProvider = ''
+getBlockIndexMethod = ''
+getBlockwithHashMethod = ''
+getTx = ''
+
+apiProvider = 'api.coingecko.com'
+
+#
+daemonCli = "litecoin-cli"
