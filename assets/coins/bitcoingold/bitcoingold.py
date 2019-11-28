cf36c191c211d70801507ab35d3cab063b4d22f5
pr0logas <prologas@protonmail.com>
2019-11-28 20:27:22 +0200

    BTG integration

---
+++ assets/coins/bitcoingold/bitcoingold.py
@@ -0,0 +1,25 @@
+# Defaults
+database = 'bitcoin-gold'
+assetTicker = 'BTG'
+assetName = 'bitcoin-gold'
+genesisBlock = 1231469665
+coinGeckoStartUnixTime = 1510099200
+blockTime = 600
+
+# RPC
+rpcconnect = '10.10.100.201'
+rpcport = 11111
+rpcuser = 'grepblock'
+rpcpassword = 'tothemoon'
+
+parseBlocksInRangeFor = 99
+
+chainProvider = 'https://btgexplorer.com/'
+getBlockIndexMethod = 'api/block-index/'
+getBlockwithHashMethod = 'api/block/'
+getTx = 'api/tx/'
+
+apiProvider = 'api.coingecko.com'
+
+#
+daemonCli = "bitcoingold-cli"
