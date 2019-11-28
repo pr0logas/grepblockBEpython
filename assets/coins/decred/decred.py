8234a94cb7923694075f3f96a739cac243e376f3
pr0logas <prologas@protonmail.com>
2019-11-28 20:48:37 +0200

    DCR implementation

---
+++ assets/coins/decred/decred.py
@@ -0,0 +1,25 @@
+# Defaults
+database = 'decred'
+assetTicker = 'DCR'
+assetName = 'decred'
+genesisBlock = 1454954535
+coinGeckoStartUnixTime = 1455062400
+blockTime = 300
+
+# RPC
+rpcconnect = '10.10.100.201'
+rpcport = 11111
+rpcuser = 'grepblockuser'
+rpcpassword = 'grepblocktothemoon'
+
+parseBlocksInRangeFor = 99
+
+chainProvider = 'https://mainnet.decred.org/'
+getBlockIndexMethod = 'api/block-index/'
+getBlockwithHashMethod = 'api/block/'
+getTx = 'api/tx/'
+
+apiProvider = 'api.coingecko.com'
+
+#
+daemonCli = "decred-cli"
