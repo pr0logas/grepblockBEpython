940185a6c199cf5854e6ed4b0abf53caf4cc2c25
Tomas Andriekus <tomas@dappradar.com>
2019-11-29 13:51:01 +0200

    Horizen debugging

--- parseBlocks.py
+++ parseBlocks.py
@@ -30,7 +30,7 @@ class aggregateBlocksData():
 				firstObj['chainlock'] = str(firstObj['chainlock'])
 
 			if 'monitored' in firstObj:
-				firstObj['monitored'] = str(firstObj['monitored'])
+				firstObj['monitored'] = str("true")
 
 			return json.dumps(firstObj)
 		else:
