9654fdd0bebcdb9e38a2ad41a916ff25dd0e0933
Tomas Andriekus <tomas@dappradar.com>
2019-11-29 13:23:00 +0200

    Horizen added

---
+++ assets/coins/horizen/horizen.py
@@ -0,0 +1,25 @@
+# Defaults
+database = "horizen"
+assetTicker = "ZEN"
+assetName = "horizen"
+genesisBlock = 1478414242
+coinGeckoStartUnixTime = 1500076800
+blockTime = 150
+
+# RPC
+rpcconnect = "10.10.100.28"
+rpcport = 18231
+rpcuser = "grepblockuser"
+rpcpassword = "grepblocktothemoon"
+
+parseBlocksInRangeFor = 99
+
+chainProvider = 'https://explorer.horizen.cc'
+getBlockIndexMethod = '/api/getblockhash?index='
+getBlockwithHashMethod = ''
+getTx = ''
+
+apiProvider = 'api.coingecko.com'
+
+#
+daemonCli = "zen-cli"
