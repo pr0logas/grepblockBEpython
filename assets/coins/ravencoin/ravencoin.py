2389305e4977abb48b92273ae6561e8a50c630c1
Tomas Andriekus <tomas@dappradar.com>
2019-11-29 12:41:15 +0200

    Ravencoin add

---
+++ assets/coins/ravencoin/ravencoin.py
@@ -0,0 +1,25 @@
+# Defaults
+database = "ravencoin"
+assetTicker = "RVN"
+assetName = "ravencoin"
+genesisBlock = 1515015723
+coinGeckoStartUnixTime = 1521590400
+blockTime = 60
+
+# RPC
+rpcconnect = "10.10.100.201"
+rpcport = 8766
+rpcuser = "grepblock"
+rpcpassword = "tothemoon"
+
+parseBlocksInRangeFor = 99
+
+chainProvider = ''
+getBlockIndexMethod = ''
+getBlockwithHashMethod = ''
+getTx = ''
+
+apiProvider = 'api.coingecko.com'
+
+#
+daemonCli = "ravencoin-cli"
