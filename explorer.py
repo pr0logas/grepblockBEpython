b72de2c7916534bcfe3c593b2da8ff58a14733c1
Tomas Andriekus <tomas@dappradar.com>
2019-11-29 12:24:16 +0200

    RDD getLAstblock alternative

--- explorer.py
+++ explorer.py
@@ -132,6 +132,8 @@ class insightExplorer(iquidusExplorer):
 	def getLastBlockAlternative(self):
 		url = (self.chainProvider+'api/status?q=getLastBlockHash')
 		req = self.u.Request(url, headers=self.header)
+		page = self.u.urlopen(req)
+		print page
 		try:
 		    page = self.u.urlopen(req)
 		except:
