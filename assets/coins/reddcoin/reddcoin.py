ef3d5fb4656e134b7c5bab7bfed14279e3696ea1
Tomas Andriekus <tomas@dappradar.com>
2019-11-29 12:20:25 +0200

    Reddcoin Added

---
+++ assets/coins/reddcoin/reddcoin.py
@@ -0,0 +1,25 @@
+# Defaults
+database = 'reddcoin'
+assetTicker = 'RDD'
+assetName = 'reddcoin'
+genesisBlock = 1390771894
+coinGeckoStartUnixTime = 1391990400
+blockTime = 60
+
+# RPC
+rpcconnect = '10.10.100.201'
+rpcport = 11111
+rpcuser = 'grepblockuser'
+rpcpassword = 'grepblocktothemoon'
+
+parseBlocksInRangeFor = 99
+
+chainProvider = 'https://live.reddcoin.com'
+getBlockIndexMethod = 'api/block-index/'
+getBlockwithHashMethod = 'api/block/'
+getTx = 'api/tx/'
+
+apiProvider = 'api.coingecko.com'
+
+#
+daemonCli = "reddcoin-cli"
