9eebba13cb3a1b024a72a7bd6375dbf1cebc53e0
Tomas Andriekus <tomas@dappradar.com>
2019-11-29 13:16:00 +0200

    Snowgem add

---
+++ assets/coins/snowgem/snowgem.py
@@ -0,0 +1,25 @@
+# Defaults
+database = 'snowgem'
+assetTicker = 'XSG'
+assetName = 'snowgem'
+genesisBlock = 1514034481
+coinGeckoStartUnixTime = 1522540800
+blockTime = 120
+
+# RPC
+rpcconnect = '10.10.100.201'
+rpcport = 16112
+rpcuser = 'grepblock'
+rpcpassword = 'tothemoon'
+
+parseBlocksInRangeFor = 99
+
+chainProvider = 'https://explorer.snowgem.org/'
+getBlockIndexMethod = 'api/block-index/'
+getBlockwithHashMethod = 'api/block/'
+getTx = 'api/tx/'
+
+apiProvider = 'api.coingecko.com'
+
+#
+daemonCli = "snowgem-cli"
