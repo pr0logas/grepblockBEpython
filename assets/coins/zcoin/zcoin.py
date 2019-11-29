c5c4149b3bedde190d12d24c863d9072f37901a9
Tomas Andriekus <tomas@dappradar.com>
2019-11-29 13:29:00 +0200

    Zcoin added

---
+++ assets/coins/zcoin/zcoin.py
@@ -0,0 +1,25 @@
+# Defaults
+database = "zcoin"
+assetTicker = "XZC"
+assetName = "zcoin"
+genesisBlock = 1475020813
+coinGeckoStartUnixTime = 1476057600
+blockTime = 300
+
+# RPC
+rpcconnect = "10.10.100.201"
+rpcport = 8888
+rpcuser = "grepblock"
+rpcpassword = "tothemoon"
+
+parseBlocksInRangeFor = 99
+
+chainProvider = ''
+getBlockIndexMethod = ''
+getBlockwithHashMethod = ''
+getTx = ''
+
+apiProvider = 'api.coingecko.com'
+
+#
+daemonCli = "zcoin-cli"
