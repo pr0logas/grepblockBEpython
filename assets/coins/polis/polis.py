c6c12965af90de5f009865a92faccf2626998da9
Tomas Andriekus <tomas@dappradar.com>
2019-11-29 12:13:57 +0200

    Polis added

---
+++ assets/coins/polis/polis.py
@@ -0,0 +1,25 @@
+# Defaults
+database = "polis"
+assetTicker = "POLIS"
+assetName = "polis"
+genesisBlock = 1513863372
+coinGeckoStartUnixTime = 1516838400
+blockTime = 120
+
+# RPC
+rpcconnect = "10.10.100.201"
+rpcport = 24127
+rpcuser = "grepblock"
+rpcpassword = "tothemoon"
+
+parseBlocksInRangeFor = 99
+
+chainProvider = ''
+getBlockIndexMethod = ''
+getBlockwithHashMethod = ''
+getTx = ''
+
+apiProvider = 'api.coingecko.com'
+
+#
+daemonCli = "polis-cli"
