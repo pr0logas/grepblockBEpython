48e3f6586b45305a27a3003179a157a8b06c29b1
pr0logas <prologas@protonmail.com>
2019-12-03 22:49:12 +0200

    BitcoinCash

---
+++ assets/coins/bitcoincash/bitcoincashcash.py
@@ -0,0 +1,29 @@
+#:: By GrepBlock.com developers // pr0logas
+#:: Modified date: 2019-11-30
+#:: Description: This file contains default values of the asset
+
+# Defaults
+database = 'bitcoin-cash'
+assetTicker = 'BCH'
+assetName = 'bitcoin-cash'
+genesisBlock = 1231469665
+coinGeckoStartUnixTime = 1501718400
+blockTime = 600
+
+# RPC
+rpcconnect = '10.10.100.201'
+rpcport = 18332
+rpcuser = 'grepblockuser'
+rpcpassword = 'grepblocktothemoon'
+
+parseBlocksInRangeFor = 99
+
+chainProvider = 'https://blockdozer.com/'
+getBlockIndexMethod = 'api/block-index/'
+getBlockwithHashMethod = 'api/block/'
+getTx = 'api/tx/'
+
+apiProvider = 'api.coingecko.com'
+
+#
+daemonCli = "bitcoincash-cli"
