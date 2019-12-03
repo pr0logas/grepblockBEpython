ad3b68dab21d32b296151ea4623026099e02e7a9
pr0logas <prologas@protonmail.com>
2019-12-03 22:19:45 +0200

    Debugging BTC graph off

--- mongoDB.py
+++ mongoDB.py
@@ -84,9 +84,7 @@ class mongoConnection():
 
 	@autoreconnect_retry
 	def findLastMarketCap(self, fromCollection, unixTime):
-		print unixTime
 		s = list(self.mongoDB[fromCollection].find({'unix_time' : unixTime}).limit(1))
-		print s
 		r = s[0]['market_data']['market_cap']['usd']
 		if r is None:
 			print('WARNING there are no historical price for unixTime: ' + str(unixTime))
