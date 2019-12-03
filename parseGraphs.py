3937aa0ee28f67706a0169b2450d6c09c5de8e90
Tomas Andriekus <tomas@dappradar.com>
2019-12-03 12:41:47 +0200

    Debugging create graph price debugging

--- parseGraphs.py
+++ parseGraphs.py
@@ -188,7 +188,7 @@ class parseGraph():
 			file = open(self.path, "r")
 			content = (file.read())
 			cjson = json.loads(content)
-			cjson[0].append(new)
+			cjson.append(new)
 			return json.dumps(cjson)
 		except:
 			print "FATAL! Failed to append the data to price graph!"
