4e40a7bb77b873f42135abdb5203a35e2c6995ac
Tomas Andriekus <tomas@dappradar.com>
2019-12-03 10:14:03 +0200

    If statement

--- parsePrices.py
+++ parsePrices.py
@@ -40,6 +40,8 @@ class parseCoinGeckoPrices():
 
 		if secObj['roi'] is None:
 			secObj['roi'] = str('null')
+		elif secObj['roi'] is 'null':
+			secObj['roi'] = str('null')
 		
 		if secObj['high_24h'] is None:
 			secObj['high_24h'] = str('null')
