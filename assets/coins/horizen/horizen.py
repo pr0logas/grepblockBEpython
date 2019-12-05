8e1a36f2859e6b7df903f96fe84b66a9f2b51f1e
Tomas Andriekus <tomas@dappradar.com>
2019-12-05 13:20:00 +0200

    Horizen

--- assets/coins/horizen/horizen.py
+++ assets/coins/horizen/horizen.py
@@ -23,7 +23,22 @@ getBlockIndexMethod = 'api/block-index/'
 getBlockwithHashMethod = 'api/block/'
 getTx = 'api/tx/'
 
-apiProvider = 'api.coingecko.com'
+apiProvider = 'https://api.coingecko.com'
+vsCurrencyUSD = 'usd'
+collectionForPricesUSD = 'priceDataUSD'
+collectionForHistoricalPricesUSD = 'historicalPriceData'
+
+# Stats related
+fileForBlockCount="assetBlocks.json"
+fileForActiveWalletsCount="assetActiveWallets.json"
+fileForBlockchainSize="assetBlockchainSize.json"
+fileForDifficulty="assetDifficulty.json"
+fileForTransactions="assetTransactions.json"
+
+# Money related
+fileForMarketCap="assetMarketCap.json"
+fileForPrice="assetPrice.json"
+fileForVolume="assetVolume.json"
 
 #
 daemonCli = "zen-cli"
