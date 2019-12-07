6863af4f492d6076d55fd93b8dd420ea239228eb
pr0logas <prologas@protonmail.com>
2019-12-07 19:26:46 +0200

    SmartCash Explorer aggresive rate limit

---
+++ assets/coins/smartcash/smartcash.py
@@ -0,0 +1,44 @@
+#:: By GrepBlock.com developers // pr0logas
+#:: Modified date: 2019-11-30
+#:: Description: This file contains default values of the asset
+
+# Defaults
+database = "smartcash"
+assetTicker = "SMART"
+assetName = "smartcash"
+genesisBlock =
+coinGeckoStartUnixTime =
+blockTime =
+
+# RPC
+rpcconnect = "10.10.100.201"
+rpcport = 8888
+rpcuser = "grepblock"
+rpcpassword = "tothemoon"
+
+parseBlocksInRangeFor = 99
+
+chainProvider = 'https://explorer.smartcash.cc/'
+getBlockIndexMethod = 'api/block-index/'
+getBlockwithHashMethod = 'api/block/'
+getTx = 'api/tx/'
+
+apiProvider = 'https://api.coingecko.com'
+vsCurrencyUSD = 'usd'
+collectionForPricesUSD = 'priceDataUSD'
+collectionForHistoricalPricesUSD = 'historicalPriceData'
+
+# Stats related
+fileForBlockCount="assetBlocks.json"
+fileForActiveWalletsCount="assetActiveWallets.json"
+fileForBlockchainSize="assetBlockchainSize.json"
+fileForDifficulty="assetDifficulty.json"
+fileForTransactions="assetTransactions.json"
+
+# Money related
+fileForMarketCap="assetMarketCap.json"
+fileForPrice="assetPrice.json"
+fileForVolume="assetVolume.json"
+
+#
+daemonCli = "zcoin-cli"
