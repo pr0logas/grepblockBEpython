8d317958f3ed5c346fd3dcdf384559aed78c18de
pr0logas <prologas@protonmail.com>
2019-12-07 17:51:53 +0200

    HP check

--- parseHistoricalPrices.py
+++ parseHistoricalPrices.py
@@ -20,6 +20,12 @@ class parseCoinGeckoHistoricalPrices():
 			   'Connection': 'keep-alive'}
 
 	def parseHistoricalPrice(self, time):
+		check1Convert = (datetime.utcfromtimestamp(time).strftime('%Y%m%d'))
+		check2Convert = datetime.now().strftime('%Y%m%d')
+		if int(check1Convert) >= int(check2Convert):
+			print('Stopping Historical Price Parsing. Reached today\'s day.')
+			sys.exit(1)
+
 		formatTime = (datetime.utcfromtimestamp(time).strftime('%d-%m-%Y'))
 		url = (self.apiProvider+ '/api/v3/coins/' + self.assetName + '/history?date=' + str(formatTime) + 
 		'&localization=false')
