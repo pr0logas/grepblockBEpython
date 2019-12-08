352b7464b1b93ea74ab30025cb8d7c9a7c90f98e
pr0logas <prologas@protonmail.com>
2019-12-08 21:39:50 +0200

    BiblePay integration

---
+++ assets/coins/biblepay/biblepay.py
@@ -0,0 +1,43 @@
+#:: By GrepBlock.com developers // pr0logas
+#:: Modified date: 2019-11-30
+#:: Description: This file contains default values of the asset
+
+# Defaults
+database = "biblepay"
+assetTicker="BBP"
+assetName = "biblepay"
+genesisBlock = 1500844608
+coinGeckoStartUnixTime = 1512604800
+blockTime = 420
+
+# RPC
+rpcconnect = "10.10.100.201"
+rpcport = 9078
+rpcuser = "grepblock"
+rpcpassword = "tothemoon"
+
+parseBlocksInRangeFor = 99
+
+chainProvider = 'http://explorer.biblepay.org'
+getBlockIndexMethod = '/api/getblockhash?index='
+getBlockwithHashMethod = '/api/getblock?hash='
+getTx = '/api/getrawtransaction?txid='
+
+apiProvider = 'https://api.coingecko.com'
+vsCurrencyUSD = 'usd'
+collectionForPricesUSD = 'priceDataUSD'
+collectionForHistoricalPricesUSD = 'historicalPriceData'
+
+# Stats related
+fileForBlockCount="assetBlocks.json"
+fileForActiveWalletsCount="assetActiveWallets.json"
+fileForBlockchainSize="assetBlockchainSize.json"
+fileForDifficulty="assetDifficulty.json"
+fileForTransactions="assetTransactions.json"
+
+# Money related
+fileForMarketCap="assetMarketCap.json"
+fileForPrice="assetPrice.json"
+fileForVolume="assetVolume.json"
+
+daemonCli = "biblepay-cli"
