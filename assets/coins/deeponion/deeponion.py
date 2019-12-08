77b50215d4cbb9c1a69c1336f21a7dc6ca8a0834
pr0logas <prologas@protonmail.com>
2019-12-08 22:02:33 +0200

    Deeponion integration

---
+++ assets/coins/deeponion/deeponion.py
@@ -0,0 +1,43 @@
+#:: By GrepBlock.com developers // pr0logas
+#:: Modified date: 2019-11-30
+#:: Description: This file contains default values of the asset
+
+# Defaults
+database = "deeponion"
+assetTicker="ONION"
+assetName = "deeponion"
+genesisBlock = 1499877016
+coinGeckoStartUnixTime = 1510704000
+blockTime = 60
+
+# RPC
+rpcconnect = "10.10.100.201"
+rpcport = 9078
+rpcuser = "grepblock"
+rpcpassword = "tothemoon"
+
+parseBlocksInRangeFor = 99
+
+chainProvider = 'https://explorer.deeponion.org'
+getBlockIndexMethod = '/api/getblockhash?index='
+getBlockwithHashMethod = '/api/getblock?hash='
+getTx = '/api/getrawtransaction?txid='
+
+apiProvider = 'https://api.coingecko.com'
+vsCurrencyUSD = 'usd'
+collectionForPricesUSD = 'priceDataUSD'
+collectionForHistoricalPricesUSD = 'historicalPriceData'
+
+# Stats related
+fileForBlockCount="assetBlocks.json"
+fileForActiveWalletsCount="assetActiveWallets.json"
+fileForBlockchainSize="assetBlockchainSize.json"
+fileForDifficulty="assetDifficulty.json"
+fileForTransactions="assetTransactions.json"
+
+# Money related
+fileForMarketCap="assetMarketCap.json"
+fileForPrice="assetPrice.json"
+fileForVolume="assetVolume.json"
+
+daemonCli = "deeponion-cli"
