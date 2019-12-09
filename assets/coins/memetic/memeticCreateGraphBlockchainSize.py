eb161e6e3a59e1c76b35e0b05a5eef4038f9d139
Tomas Andriekus <tomas@dappradar.com>
2019-12-09 18:49:28 +0200

    Memetic

---
+++ assets/coins/memetic/memeticCreateGraphBlockchainSize.py
@@ -0,0 +1,92 @@
+#:: By GrepBlock.com developers // pr0logas, mrNemo
+#:: Modified date: 2019-11-30
+#:: Description: This file is a workspace for assetGraph creation.
+
+import sys, time
+from datetime import datetime, timedelta
+from time import gmtime, strftime
+from memetic import *
+sys.path.append('../../../')
+from mongoDB import *
+from parseGraphs import parseGraph
+
+db = database
+collectionForBlocks = "blocks"
+
+# Init Classes;
+PG = parseGraph(assetTicker, fileForBlockchainSize, genesisBlock)
+MC = mongoConnection(mongoAuth, db, collectionForBlocks)
+
+# Find Last unixTime value in a working json file;
+lU = PG.parseBlockchainSizeFindLastValueTime()
+if lU == 'FileWasEmpty!':
+	lU = PG.parseBlockchainSizeFindLastValueTime()
+	print("Warning, file was empty, init zero params!")
+
+# Find the same but in MongoDB;
+lastBlockByUnixTime = MC.findLastBlockTime(collectionForBlocks, lU)
+
+# Last Block value in mongoDB;
+findLastBlock = MC.findLastBlock(collectionForBlocks)
+
+# Init Global while vars;
+nextDayTime = (datetime.fromtimestamp(float(lU)) + timedelta(hours=24)).strftime('%Y-%m-%d') # Increase 1 day;
+sumSize = PG.parseBlockchainSizeFindLastValueSize()
+nextDayTimeWhileProgress = nextDayTime
+
+whileprogress = lastBlockByUnixTime
+while whileprogress <= findLastBlock:
+	lB = MC.findByBlock(collectionForBlocks, whileprogress)
+	if lB != []: # This should never happen!
+		count = lB['size']
+		unixTime = lB['time']
+		reqNum = int(count)
+		currBlkTime = (datetime.fromtimestamp(unixTime)).strftime('%Y-%m-%d')
+
+		timeSet = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+
+		# This should never happen. But if the blockchain stopped for more than 24h?
+		check1 = str(currBlkTime).replace("-", "")
+		check2 = str(nextDayTimeWhileProgress).replace("-", "")
+
+		if int(check1) > int(check2):
+			print("WARNING! The blockchain STALL has been detected!!!")
+			printTime = (datetime.fromtimestamp(unixTime)).strftime('%Y-%m-%d')
+			timeSet = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+			resJSON = PG.appendNewContentToBlockchainSizeGraph(sumSize, unixTime)
+			resWrite = PG.writeJSONtoFile(resJSON)
+			if resWrite == 'OK':
+				print(timeSet + " Next day found. Total BlockchainSize: " + str(sumSize) + " bytes. // We at " + str(printTime))
+				nextDayTimeWhileProgress = (datetime.fromtimestamp(unixTime) + timedelta(hours=24)).strftime('%Y-%m-%d') # Increase 1 day;
+			else:
+				print("FATAL!")
+				sys.exit(1)
+
+		elif currBlkTime != nextDayTimeWhileProgress:
+			sumSize = (int(reqNum) + int(sumSize))
+
+		else:
+			printTime = (datetime.fromtimestamp(unixTime)).strftime('%Y-%m-%d')
+			timeSet = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+			resJSON = PG.appendNewContentToBlockchainSizeGraph(sumSize, unixTime)
+			resWrite = PG.writeJSONtoFile(resJSON)
+			if resWrite == 'OK':
+				print(timeSet + " Next day found. Total BlockchainSize: " + str(sumSize) + " bytes. // We at " + str(printTime))
+				nextDayTimeWhileProgress = (datetime.fromtimestamp(unixTime) + timedelta(hours=24)).strftime('%Y-%m-%d') # Increase 1 day;
+			else:
+				print("FATAL!")
+				sys.exit(1)
+
+	else:
+		print("FATAL! Something went wrong while counting BlockchainSize Graph!")
+		sys.exit(1)
+
+	whileprogress += 1
+
+# Send new JSON to FE;
+PG.sendJSONtoFronend()
+timeSet = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+print(timeSet +" ***JSON copied to FE instance***")
+
+timeSet = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+print(timeSet +" All tasks were successful.")
---
+++ assets/coins/memetic/memeticImportBlocks.py
@@ -0,0 +1,88 @@
+#:: By GrepBlock.com developers // pr0logas
+#:: Modified date: 2019-11-30
+#:: Description: This file is a workspace for block importation.
+
+import sys, time
+from time import gmtime, strftime
+from memetic import *
+sys.path.append('../../../')
+from mongoDB import *
+from explorer import iquidusExplorer
+from parseBlocks import aggregateBlocksData
+
+db = database
+collectionForBlocks = "blocks"
+
+# Init Classes;
+MC = mongoConnection(mongoAuth, db, collectionForBlocks)
+EX = iquidusExplorer(chainProvider, getBlockIndexMethod, getBlockwithHashMethod, getTx)
+AG = aggregateBlocksData()
+
+# Check if blocks col empty or not?
+if MC.checkIfBlocksColEmpty(collectionForBlocks) == "Empty":
+	print "Warning! We found an empty blocks collection. Starting from zero."
+	MC.insertInitValueForBlocks(collectionForBlocks)
+
+# Set current progress;
+currentLastBlock = MC.findLastBlock(collectionForBlocks)
+
+# Check last Explorer block:
+currentExplBlock = int(EX.getLastBlock())
+currentExplBlock -= 2
+
+# Set how much blocks we want to sync from current point +- ~99
+parsingBlocksInRange = parseBlocksInRangeFor + currentLastBlock
+
+# Check if our progress is near by Explorer blocks?
+diff = str(currentExplBlock - currentLastBlock)
+
+# We have two choises here, parse the blocks in range for +- ~100 blocks (last block too far anyway) or until last Explorer block -2 //
+# We don't wanna to parse up to last block in case of wrong chain.
+
+# Start Parsing blocks ::in range:: and push to MongoDB;
+if (int(diff) >= 100):
+	whileprogress = currentLastBlock 
+
+	if whileprogress == 0:
+		whileprogress += 1
+
+	while whileprogress < parsingBlocksInRange:
+		setProcStart = int(round(time.time() * 1000))
+		bH = EX.getBlockHash(str(whileprogress))
+		bD = EX.getBlockContentByHash(bH)
+		aggregatedBlockData = AG.aggregateInsertBlockNumber(bD)
+		status = MC.insertBlocksData(collectionForBlocks, aggregatedBlockData)
+		whileprogress += 1
+
+		setProcEnd = int(round(time.time() * 1000))
+		performanceResult = str(setProcEnd - setProcStart)
+		timeSet = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+		if str(status) != 'None':
+			print timeSet + " Block finished: " + str(status) + ' // ' + (performanceResult) + ' ms'
+
+	timeSet = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+	print timeSet + " No new blocks found, last one: " + str(parsingBlocksInRange)
+
+# Start Parsing blocks until last Explorer block -2 and push to MongoDB;
+else: 
+	whileprogress = currentLastBlock 
+
+	if whileprogress == 0:
+		whileprogress += 1
+
+	while whileprogress < currentExplBlock:
+		setProcStart = int(round(time.time() * 1000))
+		bH = EX.getBlockHash(str(whileprogress))
+		bD = EX.getBlockContentByHash(bH)
+		aggregatedBlockData = AG.aggregateInsertBlockNumber(bD)
+		status = MC.insertBlocksData(collectionForBlocks, aggregatedBlockData)
+		whileprogress += 1
+
+		setProcEnd = int(round(time.time() * 1000))
+		performanceResult = str(setProcEnd - setProcStart)
+		timeSet = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+		if str(status) != 'None':
+			print timeSet + " Block finished: " + str(status) + ' // ' + (performanceResult) + ' ms'
+
+	timeSet = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+	print timeSet + " No new blocks found, last one: " + str(currentExplBlock)
