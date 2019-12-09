05d4d5cc60e9ae65a2813eea7077a651ac516973
Tomas Andriekus <tomas@dappradar.com>
2019-12-09 15:43:16 +0200

    Deviant

---
+++ assets/coins/deviant/deviant.py
@@ -0,0 +1,43 @@
+#:: By GrepBlock.com developers // pr0logas
+#:: Modified date: 2019-11-30
+#:: Description: This file contains default values of the asset
+
+# Defaults
+database = "deviant"
+assetTicker="ADE"
+assetName = "deviant"
+genesisBlock = 1529783629
+coinGeckoStartUnixTime = 1522368000
+blockTime = 60
+
+# RPC
+rpcconnect = "10.10.100.201"
+rpcport = 9078
+rpcuser = "grepblock"
+rpcpassword = "tothemoon"
+
+parseBlocksInRangeFor = 99
+
+chainProvider = 'http://explorer.deviantcoin.io'
+getBlockIndexMethod = '/api/getblockhash?index='
+getBlockwithHashMethod = '/api/getblock?hash='
+getTx = '/api/getrawtransaction?txid='
+
+apiProvider = 'https://api.coingecko.com'
+vsCurrencyUSD = 'usd'
+collectionForPricesUSD = 'priceDataUSD'
+collectionForHistoricalPricesUSD = 'historicalPriceData'
+
+# Stats related
+fileForBlockCount="assetBlocks.json"
+fileForActiveWalletsCount="assetActiveWallets.json"
+fileForBlockchainSize="assetBlockchainSize.json"
+fileForDifficulty="assetDifficulty.json"
+fileForTransactions="assetTransactions.json"
+
+# Money related
+fileForMarketCap="assetMarketCap.json"
+fileForPrice="assetPrice.json"
+fileForVolume="assetVolume.json"
+
+daemonCli = "deviant-cli"
