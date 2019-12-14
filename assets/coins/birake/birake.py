ac8cbc3dc8f7f008547e551b5666a17b2f05688e
roothere <roothere@protonmail.com>
2019-12-14 19:23:05 +0200

    Birake expl update

--- assets/coins/birake/birake.py
+++ assets/coins/birake/birake.py
@@ -18,7 +18,7 @@ rpcpassword = "tothemoon"
 
 parseBlocksInRangeFor = 99
 
-chainProvider = 'http://explorer.birake.com/'
+chainProvider = 'http://explorer.birake.com'
 getBlockIndexMethod = '/api/getblockhash?index='
 getBlockwithHashMethod = '/api/getblock?hash='
 getTx = '/api/getrawtransaction?txid='
