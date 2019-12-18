10a471b988c897c6db3e5ae4990ad544f06b8aa5
roothere <roothere@protonmail.com>
2019-12-18 20:05:05 +0200

    Telos (TELOS)

---
+++ assets/coins/telos/telos.py
@@ -0,0 +1,43 @@
+#:: By GrepBlock.com developers // pr0logas
+#:: Modified date: 2019-12-18
+#:: Description: This file contains default values of the asset
+
+# Defaults
+database = "telos"
+assetTicker="TELOS"
+assetName = "telos"
+genesisBlock = 1531879390
+coinGeckoStartUnixTime = 1535241600
+blockTime = 66
+
+# RPC
+rpcconnect = "10.10.100.201"
+rpcport = 9078
+rpcuser = "grepblock"
+rpcpassword = "tothemoon"
+
+parseBlocksInRangeFor = 99
+
+chainProvider = 'http://159.69.33.243:3001'
+getBlockIndexMethod = '/api/getblockhash?index='
+getBlockwithHashMethod = '/api/getblock?hash='
+getTx = '/api/getrawtransaction?txid='
+
+apiProvider = 'https://api.coingecko.com'
+vsCurrencyUSD = 'usd'
+collectionForPricesUSD = 'priceDataUSD'
+collectionForHistoricalPricesUSD = 'historicalPriceData'
+
+# Stats related
+fileForBlockCount="assetBlocks.json"
+fileForActiveWalletsCount="assetActiveWallets.json"
+fileForBlockchainSize="assetBlockchainSize.json"
+fileForDifficulty="assetDifficulty.json"
+fileForTransactions="assetTransactions.json"
+
+# Money related
+fileForMarketCap="assetMarketCap.json"
+fileForPrice="assetPrice.json"
+fileForVolume="assetVolume.json"
+
+daemonCli = "telos-cli"
