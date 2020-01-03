f2dbdaa05451146eaabe4b18024ec46944bdaba0
Tomas Andriekus <tomas@dappradar.com>
2020-01-03 21:46:11 +0200

    Tron backbone

---
+++ assets/platforms/tron/tron.py
@@ -0,0 +1,32 @@
+#:: By GrepBlock.com developers // pr0logas
+#:: Modified date: 2019-12-31
+#:: Description: This file contains default values of the asset platform
+
+# Defaults
+database = "tron"
+assetTicker="ETH"
+assetName = "tron"
+genesisBlock = 1438269988
+coinGeckoStartUnixTime = 1440028800
+blockTime = 15
+
+parseBlocksInRangeFor = 999
+
+chainProvider = 'http://116.202.49.92:8545'
+
+apiProvider = 'https://api.coingecko.com'
+vsCurrencyUSD = 'usd'
+collectionForPricesUSD = 'priceDataUSD'
+collectionForHistoricalPricesUSD = 'historicalPriceData'
+
+# Stats related
+fileForBlockCount="assetBlocks.json"
+fileForActiveWalletsCount="assetActiveWallets.json"
+fileForBlockchainSize="assetBlockchainSize.json"
+fileForDifficulty="assetDifficulty.json"
+fileForTransactions="assetTransactions.json"
+
+# Money related
+fileForMarketCap="assetMarketCap.json"
+fileForPrice="assetPrice.json"
+fileForVolume="assetVolume.json"
---
+++ assets/platforms/tron/tronImportHistoricalPrices.py
@@ -0,0 +1,41 @@
+#:: By GrepBlock.com developers // pr0logas
+#:: Modified date: 2019-11-30
+#:: Description: This file is a workspace for Historical Prices importation.
+
+import sys, time
+from datetime import datetime, timedelta
+from time import gmtime, strftime
+from tron import *
+sys.path.append('../../../')
+from mongoDB import *
+from parseHistoricalPrices import parseCoinGeckoHistoricalPrices
+
+db = database
+col = collectionForHistoricalPricesUSD
+coingckstart = coinGeckoStartUnixTime
+
+# Init Classes;
+MC = mongoConnection(mongoAuth, db, col)
+PP = parseCoinGeckoHistoricalPrices(apiProvider, vsCurrencyUSD, assetName)
+
+
+# Check if blocks col empty or not?
+if MC.checkIfHistoricalPricesColEmpty(col) == "Empty":
+	print "Warning! We found an empty HistoricalPrices collection. Starting from zero."
+	MC.insertInitValueForHP(col, coingckstart)
+
+# MongoDB check last progress;
+lastTime = MC.findLastHistoricalPrices(col)
+lastTime = (int(lastTime) + 86400) # Increase 1 day
+printTime = (datetime.fromtimestamp(lastTime)).strftime('%Y-%m-%d %H:%M:%S')
+
+# CoinGecko
+result = PP.parseHistoricalPrice(lastTime)
+
+# Insert Unix Time
+aggregatedData = PP.aggregateInsertUnixTime(result, lastTime)
+
+#Insert to MongoDB
+res = MC.insertHistoricalPricesData(col, aggregatedData)
+timeSet = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+print(timeSet + " Succefully inserted asset historical price: $" + res + ' at: ' + printTime)
